5a00e45d5e68748ccaca0bea599c3b34e17481c2
Brandon Karl <brandon@bkarl.com>
2017-06-26 19:03:41 -0400

    Changed map icon

--- Lunenburg_site/homepage/assets/js/map.js
+++ Lunenburg_site/homepage/assets/js/map.js
@@ -15,6 +15,13 @@ function initMap() {
 			lat: 42.592633,
 			lng: -71.7581613
 		},
-		map: map
+		map: map,
+		icon: '<i></i>'
 	});
+
+	var infoWindow = new google.maps.InfoWindow({
+		content: '<div style="height: 50px;"><img style="max-width:100%; max-height:100%; padding-left: 25px;" src="http://i.imgur.com/u5cujkK.png"></div>'
+	});
+
+	infoWindow.open(map, marker);
 }

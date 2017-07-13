66ed69aff5b3c2111d9470ea8e6521a1e816b7c1
Connor Emma <connoremma@Connors-MacBook-Pro.local>
2017-07-12 21:13:34 -0400

    Fixed currently closed color

--- Lunenburg_site/homepage/assets/js/date.js
+++ Lunenburg_site/homepage/assets/js/date.js
@@ -4,5 +4,5 @@ if(((1 <= d.getDay() && d.getDay() <= 4) && (d.getHours() >= 21 || d.getHours() 
 	 ((5 <= d.getDay() && d.getDay() <= 6) && (d.getHours() >= 22 || d.getHours() < 11)) ||
 	 ((0 == d.getDay()) && (d.getHours() >= 21 || d.getHours() < 12))) {
 			document.getElementById("status").innerHTML = "Closed";
-			document.getElementById("status").style.color = "Red";
+			document.getElementById("status").style.color = "White";
 }
